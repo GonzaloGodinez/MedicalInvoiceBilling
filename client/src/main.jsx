24bb0ff9a825cdc9500a69b02ee7deb1dcf992cf
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2024-02-15 00:04:53 -0700

    clean up front end tabs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -25,19 +25,11 @@ const router = createBrowserRouter([
         path: "/billing",
         element: <BillingPage />
       },
-      //{
-      //   path: '/signup',
-      //   element: <Signup />
-      // }, {
-      //   path: '/profiles/:username',
-      //   element: <Profile />
-      // }, {
-      //   path: '/me',
-      //   element: <Profile />
-      // }, {
-      //   path: '/thoughts/:thoughtId',
-      //   element: <SingleThought />
-      // }
+      // {
+      //   path: "/patientInfo",
+      //   element: <PatientPage />
+      // },
+      
     ]
   },
 ]);
@@ -45,48 +37,3 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 )
-
-
-/*
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-import App from './App.jsx'
-// import Login from './components/LoginForm';
-import SearchProviders from './pages/SearchProviders.jsx'
-import BillingPage from './components/BillingPage'
-import CreatePatient from './pages/CreatePatient'
-import ErrorPage from './pages/ErrorPage'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        Index: true,
-        element: <CreatePatient />
-      },
-      {
-        Path: "/billing",
-        element: <BillingPage />
-      },
-      // login is included in the navbar.jsx file
-       {
-        path: '/provider',
-         element: <SearchProviders />
-       },
-      //  {
-      //   path: '/signup',
-      //    element: <SignUpForm />
-      //  },
-    ]
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
-*/
